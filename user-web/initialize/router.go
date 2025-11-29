0122d52104bf0cdeee9b83ab1043c689e6635e4b
wanglele <wanglele@luojilab.com>
2025-11-29 11:28:00 +0800

    Add cors middleware resolve cross origin

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	"mx-shop-api/user-web/middlewares"
 	"mx-shop-api/user-web/router"
 
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,7 @@ import (
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+	Router.Use(middlewares.Cors()) // 解决跨域
 
 	APIV1Router := Router.Group("v1")
 	router.InitUserRouter(APIV1Router)
