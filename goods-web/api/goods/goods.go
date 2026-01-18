1fcdc488e27f8f31cf72e8e8a8cba637958503de
wanglele <wanglele@luojilab.com>
2026-01-18 12:52:19 +0800

    Add get goods list api

--- goods-web/api/goods/goods.go
+++ goods-web/api/goods/goods.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -40,6 +41,7 @@ func HandleGrpcErrorToHttp(err error, ctx *gin.Context) {
 }
 
 func GoodsList(ctx *gin.Context) {
+	s := zap.S()
 	req := proto.GoodsFilterRequest{}
 	// 价格最小值
 	priceMin := ctx.DefaultQuery("pmin", "0")
@@ -50,7 +52,7 @@ func GoodsList(ctx *gin.Context) {
 	priceMaxInt, _ := strconv.Atoi(priceMax)
 	req.PriceMax = int32(priceMaxInt)
 	// 是否最热
-	isHot := ctx.DefaultQuery("in", "0")
+	isHot := ctx.DefaultQuery("ih", "0")
 	if isHot == "1" {
 		req.IsHot = true
 	}
@@ -65,14 +67,74 @@ func GoodsList(ctx *gin.Context) {
 		req.IsTab = true
 	}
 
+	categoryId := ctx.DefaultQuery("c", "0")
+	if categoryId != "0" {
+		topCategory, _ := strconv.Atoi(categoryId)
+		req.TopCategory = int32(topCategory)
+	}
+
+	page := ctx.DefaultQuery("pn", "1")
+	if page != "0" {
+		pageS, _ := strconv.Atoi(page)
+		req.Pages = int32(pageS)
+	}
+
+	pageNum := ctx.DefaultQuery("pnum", "10")
+	if pageNum != "0" {
+		pageSize, _ := strconv.Atoi(pageNum)
+		req.PagePerNums = int32(pageSize)
+	}
+
+	keywords := ctx.DefaultQuery("q", "")
+	if keywords != "" {
+		req.KeyWords = keywords
+	}
+
+	brandIdStr := ctx.DefaultQuery("b", "0")
+	if brandIdStr != "0" {
+		brand, _ := strconv.Atoi(brandIdStr)
+		req.Brand = int32(brand)
+	}
+
 	resp, err := global.GoodSrvClient.GoodsList(context.Background(), &req)
 	if err != nil {
+		s.Errorf("【GoodsList】Error", err.Error())
 		HandleGrpcErrorToHttp(err, ctx)
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
+	goodsList := make([]interface{}, 0)
+
+	for _, value := range resp.Data {
+		goodsList = append(goodsList, map[string]interface{}{
+			"id":          value.Id,
+			"name":        value.Name,
+			"goods_brief": value.GoodsBrief,
+			"desc":        value.GoodsDesc,
+			"ship_free":   value.ShipFree,
+			"images":      value.Images,
+			"desc_images": value.DescImages,
+			"front_image": value.GoodsFrontImage,
+			"shop_price":  value.ShopPrice,
+			"category": map[string]interface{}{
+				"id":   value.Category.Id,
+				"name": value.Category.Name,
+			},
+			"brand": map[string]interface{}{
+				"id":   value.Brand.Id,
+				"name": value.Brand.Name,
+				"logo": value.Brand.Logo,
+			},
+			"is_hot":  value.IsHot,
+			"is_new":  value.IsNew,
+			"on_sale": value.OnSale,
+		})
+	}
+
+	respMap := map[string]interface{}{
 		"total": resp.Total,
-		"list":  resp.Data,
-	})
+		"data":  goodsList,
+	}
+
+	ctx.JSON(http.StatusOK, respMap)
 }
